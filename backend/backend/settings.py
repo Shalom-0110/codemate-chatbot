77d2efe4469fe0b121e4860ac2fbf16f2d483905
Shalom-0110 <20K81A1292@smecacin.onmicrosoft.com>
2025-10-16 20:34:02 +0530

    deployment ready changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mma7mtfs#@t3a80tppvlzp+5d9+*3xho&wrn6h!o##*ngvno6w'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-for-dev-only")
+DEBUG=True
 
 ALLOWED_HOSTS = []
 
